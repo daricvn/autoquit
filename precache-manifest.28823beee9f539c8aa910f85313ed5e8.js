f6386541bf10b54b2efbc8b3dd50b3d6b8b39139
Darick Nguyen <daricvn@outlook.com>
2019-07-21 18:41:16 +0700

    First commit

---
+++ precache-manifest.28823beee9f539c8aa910f85313ed5e8.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "335040146da4507bb5300551670991b5",
+    "url": "/index.html"
+  },
+  {
+    "revision": "48a7c50d3c39ba7810a6",
+    "url": "/static/css/main.cc9e699e.chunk.css"
+  },
+  {
+    "revision": "cd0d290b87c5c80fd5ff",
+    "url": "/static/js/2.c7689427.chunk.js"
+  },
+  {
+    "revision": "48a7c50d3c39ba7810a6",
+    "url": "/static/js/main.fc8281fc.chunk.js"
+  },
+  {
+    "revision": "42ac5946195a7306e2a5",
+    "url": "/static/js/runtime~main.a8a9905a.js"
+  },
+  {
+    "revision": "7629f785e536bc72d957306a039b0a74",
+    "url": "/static/media/autoquit.7629f785.png"
+  },
+  {
+    "revision": "408d1807ac89778ca5b9a848232d3c18",
+    "url": "/static/media/guide.408d1807.mp4"
+  }
+]);
